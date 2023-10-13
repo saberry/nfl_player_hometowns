9f2512de8fdf16b69ebab8bc54ca72e923e23ab5
Seth Berry <seth.berry@nd.edu>
2023-10-13 14:17:48 -0400

    Adding position classes.

--- code/mapping_towns.R
+++ code/mapping_towns.R
@@ -19,9 +19,39 @@ all_player_df[, c("lat", "lon")] <- lapply(c("lat", "lon"), function(x) {
   as.numeric(all_player_df[, x])
 })
 
-position_cols <- colorFactor("Set3", all_player_df$position)
+all_player_df$position_type <- ""
+
+all_player_df$position_type[
+  all_player_df$position %in% c("C", "G", "OT", "OL", "LS")
+  ] <- "O line"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("SAF", "CB", "FS", "DB", "S")
+  ] <- "D backs"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("DE", "DT", "NT", "DL")
+] <- "D line"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("ILB", "LB", "MLB", "OLB")
+] <- "linebackers"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("WR", "TE")
+] <- "receivers"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("QB", "RB", "FB")
+] <- "O backs"
+
+all_player_df$position_type[
+  all_player_df$position %in% c("P", "K")
+] <- "kickers"
+
+position_cols <- colorFactor("Set3", all_player_df$position_type)
 
 leaflet(all_player_df) %>% 
   addProviderTiles(providers$CartoDB.Positron) %>% 
-  addCircles(lng = ~lon, lat = ~lat, color = ~position_cols(position)) %>% 
-  addLegend(pal = position_cols, values = ~position)
+  addCircles(lng = ~lon, lat = ~lat, color = ~position_cols(position_type)) %>% 
+  addLegend(pal = position_cols, values = ~position_type)
